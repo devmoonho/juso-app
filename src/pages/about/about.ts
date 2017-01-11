7585b9f6166ccdd139e5ef6daa527dbc4803837f
moonho yeo <yeomoonho@gmail.com>
2017-01-11 09:06:19 +0900

    about page 작성

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ModalController, Platform, NavParams, ViewController } from 'ionic-angular';
+import { ModalController, Platform, NavParams, ViewController, ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
@@ -8,11 +8,42 @@ import { ModalController, Platform, NavParams, ViewController } from 'ionic-angu
 
 export class AboutPage {
 
+  admin: any = {
+    tel: '+82-10-9968-8303',
+    email: 'yeomoonho@gmail.com',
+    linkedin: 'https://kr.linkedin.com/in/moonho-yeo-8b53b9117',
+    // about: '도로명주소 검색기능을 모바일에서 구현한 App이며 Firebase와 연동하여 사용자 관리및 즐겨찾기 구현 '
+  }
+
   constructor(
-   public viewCtrl: ViewController) {
+    public viewCtrl: ViewController,
+    private toastCtrl: ToastController
+  ) {
+
+  }
+
+  goEmail(){
+
+  }
+  
+  goCall(){
+
+  }
+
+  goLinkedin() {
+    this.displayToast("클립보드에 저장되었습니다.");
   }
 
   dismiss() {
     this.viewCtrl.dismiss();
+  }
+
+  displayToast(msg: string) {
+    let toast: any;
+    toast = this.toastCtrl.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
   }
 }
